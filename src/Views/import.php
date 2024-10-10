4ebae677212d59476f6dd4d5d4440131328f196e
DaanKlaassen <daan.klaassen@student.gildeopleidingen.nl>
2024-10-10 09:18:42 +0200

    import page toegevoegd

--- src/Views/import.php
+++ src/Views/import.php
@@ -20,136 +20,136 @@ $rootURL = $config->getRootURL();
 </head>
 
 <body>
-<div class="import-container">
-    <div class="sidebar">
-        <div class="user">
-            <div class="user-icon">
-                <img src="../img/user.svg" alt="User Icon" class="user-placeholder">
-            </div>
+    <div class="import-container">
+        <div class="sidebar">
+            <div class="user">
+                <div class="user-icon">
+                    <img src="../img/user.svg" alt="User Icon" class="user-placeholder">
+                </div>
 
-            <div class="voor-achternaam-admin">
-                <div class="voor-achternaam">
-                    Daan <br> Klaassen
+                <div class="voor-achternaam-admin">
+                    <div class="voor-achternaam">
+                        Daan <br> Klaassen
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="menu-items">
-            <a href="<?php echo $rootURL; ?>/admin">
-                <button class="menu-item">Dashboard</button>
-            </a>
-            <a href="<?php echo $rootURL; ?>/admin">
-                <button class="menu-item">Database</button>
-            </a>
-            <a href="<?php echo $rootURL; ?>/admin">
-                <button class="menu-item">Users</button>
-            </a>
-            <a href="<?php echo $rootURL; ?>/admin/import">
-                <button class="menu-item active">Import</button>
-            </a>
-            <a href="<?php echo $rootURL; ?>/admin/export">
-                <button class="menu-item">Export</button>
-            </a>
-        </div>
+            <div class="menu-items">
+                <a href="<?php echo $rootURL; ?>/admin">
+                    <button class="menu-item">Dashboard</button>
+                </a>
+                <a href="<?php echo $rootURL; ?>/admin">
+                    <button class="menu-item">Database</button>
+                </a>
+                <a href="<?php echo $rootURL; ?>/admin">
+                    <button class="menu-item">Users</button>
+                </a>
+                <a href="<?php echo $rootURL; ?>/admin/import">
+                    <button class="menu-item active">Import</button>
+                </a>
+                <a href="<?php echo $rootURL; ?>/admin/export">
+                    <button class="menu-item">Export</button>
+                </a>
+            </div>
 
-        <div class="buttons-container">
-            <button class="settings">
-                <img src="../img/settingsknop.svg" alt="settings Icon" class="settings-icon">
-            </button>
-            <button class="exit"><a href="<?php echo $rootURL; ?>/">
-                    <img src="../img/log-out.svg" alt="exit Icon" class="exit-icon"></a>
-            </button>
+            <div class="buttons-container">
+                <button class="settings">
+                    <img src="../img/settingsknop.svg" alt="settings Icon" class="settings-icon">
+                </button>
+                <button class="exit"><a href="<?php echo $rootURL; ?>/">
+                        <img src="../img/log-out.svg" alt="exit Icon" class="exit-icon"></a>
+                </button>
+            </div>
+        </div>
+        <div class="import-main-section">
+            <div class="import-section">
+                <h1>Importeer de database via een CSV bestand</h1>
+                <form id="importForm">
+                    <label for="file" class="export-label" style="display: none;"> Import file
+                    </label>
+                    <input type="file" accept=".csv" name="file" value="true" class="import-file">
+                    <button type="submit" id="importButton" class="import-button">Importeer</button>
+                </form>
+            </div>
         </div>
     </div>
-    <div class="import-main-section">
-        <div class="import-section">
-            <h1>Importeer de database via een CSV bestand</h1>
-            <form id="importForm">
-                <label class="export-label">
-                    <input type="file" name="file" value="true" class="import-file"> Import file
-                </label>
-                <button type="submit" id="importButton" class="import-button">Importeer</button>
-            </form>
-        </div>
+    <div class="logo">
+        <img src="../img/gildedevops-logo.png" alt="GildeDevOps Logo">
     </div>
-</div>
-<div class="logo">
-    <img src="../img/gildedevops-logo.png" alt="GildeDevOps Logo">
-</div>
 
-<script>
-    const importButton = document.getElementById("importButton");
-    document.getElementById('importForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-        importButton.disabled = true;
+    <script>
+        const importButton = document.getElementById("importButton");
+        document.getElementById('importForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            importButton.disabled = true;
 
-        const formData = new FormData(this);
-        const data = {
-            file: formData.get('file') || false,
-        };
-        console.log(data);
-
-        const fileInput = formData.get('file');
-        if (fileInput && fileInput.name.endsWith('.csv')) {
-            const reader = new FileReader();
-            reader.onload = async function (event) {
-                const csvData = event.target.result;
-                const lines = csvData.split('\r\n');
-                const result = [];
-                const headers = lines[0].split(',');
-
-                for (let i = 1; i < lines.length; i++) {
-                    const obj = {};
-                    const currentline = lines[i].split(',');
-
-                    for (let j = 0; j < headers.length; j++) {
-                        obj[headers[j]] = currentline[j];
-                    }
-                    result.push(obj);
-                }
-                console.log(result);
-                try {
-                    const response = await fetch('<?php echo $rootURL; ?>/import-csv', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(result)
-                    });
-
-                    const responseText = await response.text();
-
-                    let data;
-                    try {
-                        data = JSON.parse(responseText); // Parse the JSON response
-                    } catch (e) {
-                        console.error('Error parsing JSON:', e);
-                    }
-
-                    if (response.status === 200) {
-                        console.log(data); // Log the JSON response
-                        if (data.message) {
-                            alert(data.message);
-                        }
-                    } else {
-                        if (data && data.message) {
-                            const errorMessages = JSON.parse(data.message.match(/\[.*\]/)[0]);
-                            alert("An error occurred while importing data:\n" + errorMessages.join('\n'));
-                        }
-                    }
-                } catch (error) {
-                    console.error('Error:', error);
-                } finally {
-                    importButton.disabled = false;
-                }
+            const formData = new FormData(this);
+            const data = {
+                file: formData.get('file') || false,
             };
-            reader.readAsText(fileInput);
-        } else {
-            alert('Please upload a valid CSV file.');
-            importButton.disabled = false;
-        }
-    });
-</script>
+            console.log(data);
+
+            const fileInput = formData.get('file');
+            if (fileInput && fileInput.name.endsWith('.csv')) {
+                const reader = new FileReader();
+                reader.onload = async function(event) {
+                    const csvData = event.target.result;
+                    const lines = csvData.split('\r\n');
+                    const result = [];
+                    const headers = lines[0].split(',');
+
+                    for (let i = 1; i < lines.length; i++) {
+                        const obj = {};
+                        const currentline = lines[i].split(',');
+
+                        for (let j = 0; j < headers.length; j++) {
+                            obj[headers[j]] = currentline[j];
+                        }
+                        result.push(obj);
+                    }
+                    console.log(result);
+                    try {
+                        const response = await fetch('<?php echo $rootURL; ?>/import-csv', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(result)
+                        });
+
+                        const responseText = await response.text();
+
+                        let data;
+                        try {
+                            data = JSON.parse(responseText); // Parse the JSON response
+                        } catch (e) {
+                            console.error('Error parsing JSON:', e);
+                        }
+
+                        if (response.status === 200) {
+                            console.log(data); // Log the JSON response
+                            if (data.message) {
+                                alert(data.message);
+                            }
+                        } else {
+                            if (data && data.message) {
+                                const errorMessages = JSON.parse(data.message.match(/\[.*\]/)[0]);
+                                alert("An error occurred while importing data:\n" + errorMessages.join('\n'));
+                            }
+                        }
+                    } catch (error) {
+                        console.error('Error:', error);
+                    } finally {
+                        importButton.disabled = false;
+                    }
+                };
+                reader.readAsText(fileInput);
+            } else {
+                alert('Please upload a valid CSV file.');
+                importButton.disabled = false;
+            }
+        });
+    </script>
 </body>
 
 </html>
